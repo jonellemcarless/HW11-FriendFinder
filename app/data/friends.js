3b0d3adeac81191aef62b859707eb2f725a801e9
Jonelle Carless <jonellecarless@Jonelles-MBP.fios-router.home>
2019-02-17 16:24:40 -0500

    need to fix modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,39 @@
 var friends = [
+//     {
+//     "name": "Beyonce",
+//     "photo": "https://ichef.bbci.co.uk/news/660/cpsprodpb/1AEE/production/_102849860_420dd9fd-ed9c-4c75-b1fa-67d276834c01.jpg",
+//     "scores": [
+//         3,
+//         2,
+//         1,
+//         3,
+//         4,
+//         5,
+//         5,
+//         1,
+//         1,
+//         1
+//     ]
+// }
     {
-    "name": "Beyonce",
-    "photo": "https://ichef.bbci.co.uk/news/660/cpsprodpb/1AEE/production/_102849860_420dd9fd-ed9c-4c75-b1fa-67d276834c01.jpg",
+    "name": "test",
+    "photo": "http://placehold.jp/150x150.png",
     "scores": [
-        3,
-        2,
         1,
-        3,
-        4,
-        5,
-        5,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
         1,
         1,
         1
     ]
-}];
+        
+    }
+
+];
 
 module.exports = friends;
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -4,12 +4,12 @@ module.exports = function(app){
 
   //if app crashes this is why
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, '/../public/home.html'));
+    res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 
 	app.get('/survey', function(req, res){
 		
-		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+		res.sendFile(path.join(__dirname + '../public/survey1.html'));
 
 	});
 
